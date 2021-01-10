e527710d435cbc41a549374e9dc25ff1fc2a3fd7
pengin7384 <pengin7384@gmail.com>
2021-01-10 21:56:52 +0900

    Fix convention to google-style

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { normalize } from 'path';
+import {normalize} from 'path';
 
 //use env_values
 dotenv.config();
@@ -13,20 +13,20 @@ const app: express.Application = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(process.env.NODE_ENV === 'production' ? logger('combined') : logger('dev'));
 app.use(cookieParser());
 
 // Body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set('port', normalize(process.env.PORT || '3000'));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('hihi');
+    res.send('hihi');
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`!!!App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode!!!`);
+    console.log(`!!!App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode!!!`);
 });
