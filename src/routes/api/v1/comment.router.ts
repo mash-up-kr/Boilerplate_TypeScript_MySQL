31182859b9c76d199054d31596a623073b103699
god9599 <59276134+god9599@users.noreply.github.com>
2021-01-26 21:14:17 +0900

    add delete comment routing

--- src/routes/api/v1/comment.router.ts
+++ src/routes/api/v1/comment.router.ts
@@ -3,6 +3,7 @@ import * as commentController from '../../../controllers/comment.controller';
 
 const router: Router = express.Router();
 
+router.delete('/:postId/comments/:commentId', commentController.deleteComment);//DELETE api/v1/post/:postId/comments/:commentId
 router.post('/:postId/comments', commentController.addComment);//POST api/v1/post/:postId/comments
 
 export default router;
